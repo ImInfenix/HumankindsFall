64adcabd40edf74420c546bb5cb71f8bfb768f57
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-01-30 17:10:05 +0100

    Beginning of the board implementation

---
+++ Assets/Scripts/Board/Board.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap tilemap;
+
+    private Tile[,] board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TileBase[] tiles = tilemap.GetTilesBlock(tilemap.cellBounds);
+        BoundsInt bounds = tilemap.cellBounds;
+        Vector2Int size = new Vector2Int(bounds.size.x, bounds.size.y);
+        Debug.Log(size);
+        int i = 0;
+        Tile[,] boardFromTiles = new Tile[size.x, size.y];
+        MinMaxInt minMaxX = new MinMaxInt();
+        MinMaxInt minMaxY = new MinMaxInt();
+        foreach (Tile tile in tiles)
+        {
+            int x = i % size.x;
+            int y = i / size.x;
+            boardFromTiles[x, y] = tile;
+            if (tile != null)
+            {
+                minMaxX.Evaluate(x);
+                minMaxY.Evaluate(y);
+            }
+            i++;
+        }
+
+        board = new Tile[minMaxX.Count, minMaxY.Count];
+
+        for(i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                //Ajouter les tiles
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/MinMax.cs
@@ -0,0 +1,50 @@
+﻿public class MinMax<T>
+{
+    public float Min { get; private set; }
+    public float Max { get; private set; }
+
+    public float Range { get { return Max - Min; } }
+    public float Count { get { return Range + 1; } }
+
+    public MinMax()
+    {
+        Min = float.MaxValue;
+        Max = float.MinValue;
+    }
+
+    /// <summary>
+    /// Defines bounds along the new value
+    /// </summary>
+    /// <param name="value">The new value</param>
+
+    public void Evaluate(float value)
+    {
+        if (value < Min) Min = value;
+        if (value > Max) Max = value;
+    }
+}
+
+public class MinMaxInt
+{
+    public int Min { get; private set; }
+    public int Max { get; private set; }
+
+    public int Range { get { return Max - Min; } }
+    public int Count { get { return Range + 1; } }
+
+    public MinMaxInt()
+    {
+        Min = int.MaxValue;
+        Max = int.MinValue;
+    }
+
+    /// <summary>
+    /// Defines bounds along the new value
+    /// </summary>
+    /// <param name="value">The new value</param>
+    public void Evaluate(int value)
+    {
+        if (value < Min) Min = value;
+        if (value > Max) Max = value;
+    }
+}
