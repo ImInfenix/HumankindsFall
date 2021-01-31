6e850a8cf2ba7abcc2a4a9944f7bd5a1d588a42c
ArthurVigneault <arthur.vigneault@laposte.net>
2021-01-31 17:59:20 -0500

    Base
    Création du unit.cs
    encore en test

---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : Unit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Unit : MonoBehaviour
+{
+    public enum Race
+    {
+        Orc,
+        Humans
+    }
+
+    public enum Class
+    {
+        Warrior,
+        Mage
+    }
+
+    public Race race;
+    public Class clas;
+    // public Object attack;
+
+    protected int maxLife;
+    protected int life;
+    protected int maxMana;
+    protected int mana;
+    protected int armor;
+    protected int moveSpeed;
+    protected float attackSpeed;
+    protected int damage;
+
+    private Transform thisTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = transform;
+    }
+
+    protected virtual void move()
+    {
+
+    }
+
+}
