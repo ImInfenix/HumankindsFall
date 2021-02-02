9cd832a35185cb3bf20b0be891663e69e0c5e9c7
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-02-02 15:11:06 +0100

    Added sprites importer for terrain tiles

---
+++ Assets/Scripts/Editor/AssetsImporters/SpritesImporter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SpritesImporter : AssetPostprocessor
+{
+    const string terrainSpritesPath = "Assets/Textures/Board/Terrain/";
+
+    void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        if (textureImporter.assetPath.StartsWith(terrainSpritesPath))
+        {
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.filterMode = FilterMode.Point;
+
+            TextureImporterPlatformSettings textureSettings = textureImporter.GetDefaultPlatformTextureSettings();
+            textureSettings.format = TextureImporterFormat.RGBA32;
+            textureSettings.textureCompression = TextureImporterCompression.Uncompressed;
+            textureImporter.SetPlatformTextureSettings(textureSettings);
+        }
+    }
+}
