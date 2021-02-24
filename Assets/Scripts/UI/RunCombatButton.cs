4afe2b407dd77afacce5288cb867a35a1788de7e
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2021-02-24 22:17:39 +0100

    Isolated combat starting button (independent of scene structure)

---
+++ Assets/Scripts/UI/RunCombatButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunCombatButton : MonoBehaviour
+{
+    public void RunCombat()
+    {
+        if (GameManager.instance.gamestate == GameManager.GameState.Placement)
+            GameManager.instance.ConfirmPlacement();
+    }
+}
