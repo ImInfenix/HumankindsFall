afb825923b8321e643b8225d39209a69c5a34a6c
Gurdilsson <Gurdilsson@gmail.com>
2021-03-07 17:50:10 +0100

    Added map scene and camera movement on the map
    Added the map sprite, a movement functionnality and a zoom functionnality

---
+++ Assets/Scripts/Map/CameraMover.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    public void moveCamera()
+    {
+        string panel = GetPanelUnderMouse();
+        if (panel.Equals("PanelLeft"))
+        {
+            cam.transform.position += new Vector3(-0.05f, 0.0f, 0.0f);
+        }
+        if (panel.Equals("PanelRight"))
+        {
+            cam.transform.position += new Vector3(+0.05f, 0.0f, 0.0f);
+        }
+        if (panel.Equals("PanelDown"))
+        {
+            cam.transform.position += new Vector3(0.0f, -0.05f, 0.0f);
+        }
+        if (panel.Equals("PanelUp"))
+        {
+            cam.transform.position += new Vector3(0.0f, +0.05f, 0.0f);
+        }
+        if (panel.Equals("PanelUpLeft"))
+        {
+            cam.transform.position += new Vector3(-0.04f, +0.04f, 0.0f);
+        }
+        if (panel.Equals("PanelUpRight"))
+        {
+            cam.transform.position += new Vector3(+0.04f, +0.04f, 0.0f);
+        }
+        if (panel.Equals("PanelDownLeft"))
+        {
+            cam.transform.position += new Vector3(-0.04f, -0.04f, 0.0f);
+        }
+        if (panel.Equals("PanelDownRight"))
+        {
+            cam.transform.position += new Vector3(+0.04f, -0.04f, 0.0f);
+        }
+    }
+
+    public void ZoomCamera()
+    {
+        if(Input.mouseScrollDelta.y < 0 && cam.orthographicSize < 5)
+        {
+            cam.orthographicSize += 1;
+        }
+        if(Input.mouseScrollDelta.y > 0 && cam.orthographicSize > 2)
+        {
+            cam.orthographicSize -= 1;
+        }
+    }
+
+    public string GetPanelUnderMouse()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            pointerId = -1,
+        };
+
+        pointerData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        GameObject panelUnderMouse = null;
+
+        foreach (RaycastResult r in results)
+        {
+            panelUnderMouse = r.gameObject;
+            if (panelUnderMouse != null)
+                break;
+        }
+        if (results.Count == 0)
+        {
+            return "";
+        }
+        return panelUnderMouse.name;
+    }
+
+    public void Update()
+    {
+        moveCamera();
+        ZoomCamera();
+    }
+
+}
