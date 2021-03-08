d381cd03d44750d03f08d629906570e2a15a2f24
Gurdilsson <Gurdilsson@gmail.com>
2021-03-08 12:32:46 +0100

    Added textures and began markers scripts

---
+++ Assets/Scripts/Map/Marker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Marker : MonoBehaviour
+{
+    [SerializeField]
+    private List<Marker> Neighbours;
+    private bool isAccessible;
+    private bool isFinished;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite GreenFlag;
+    [SerializeField]
+    private Sprite RedFlag;
+    public void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = RedFlag;
+    }
+    public void FinishLevel()
+    {
+        spriteRenderer.sprite = GreenFlag;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            FinishLevel();
+    }
+}
